8d6543813e530c34ab201f2c6e714250ae865943
woodsection <82582199+woodsection@users.noreply.github.com>
2022-07-21 18:05:01 +0900

    Update dan chapter5

--- dan/Chapter05/main.py
+++ dan/Chapter05/main.py
@@ -1,7 +1,5 @@
 from singleton.singlethread import SingleThread
 from singleton.multiThread import MultiThread
-
-from threading import Thread
 from multiprocessing.pool import ThreadPool
 
 
@@ -9,19 +7,19 @@ class Test:
     pass
 
 if __name__ == "__main__":
-    
+
     st1 = SingleThread()
     st2 = SingleThread()
 
     print("########SingleThread########\n")
-    
+
     print("--------------SingleThread - Not safe--------------")
     print(f"st1 value = {st1.get_value()}")
     print(f"st2 value = {st2.get_value()}")
     print("---------------------------------------------------\n")
-    
+
     pool = ThreadPool(processes=8)
-    
+
     for _ in range(1, 100):
         mt1 = pool.apply_async(SingleThread)
         mt2 = pool.apply_async(SingleThread)
@@ -37,19 +35,19 @@ if __name__ == "__main__":
         del mt1
         del mt2
         del mt3
-    
+
     st1 = MultiThread()
     st2 = MultiThread()
 
     print("########MultiThread########\n")
-    
-    print("--------------MultiThread - Not safe--------------")
+
+    print("--------------MultiThread - safe-------------------")
     print(f"st1 value = {st1.get_value()}")
     print(f"st2 value = {st2.get_value()}")
     print("---------------------------------------------------\n")
-    
+
     pool = ThreadPool(processes=8)
-    
+
     for _ in range(1, 100):
         mt1 = pool.apply_async(MultiThread)
         mt2 = pool.apply_async(MultiThread)
@@ -57,7 +55,7 @@ if __name__ == "__main__":
         if mt1.get() != \
             mt2.get() != \
             mt3.get():
-            print("--------------MultiThread - Not safe---------------")
+            print("--------------MultiThread -  safe------------------")
             print(f"mt1 value = {mt1.get().get_value()}")
             print(f"mt2 value = {mt2.get().get_value()}")
             print(f"mt3 value = {mt3.get().get_value()}")
@@ -65,7 +63,4 @@ if __name__ == "__main__":
         del mt1
         del mt2
         del mt3
-    
-    
-    
-    
+
